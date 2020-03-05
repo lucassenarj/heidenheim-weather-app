f3dbb71d081dc2f97db76905e43b3dd8a929a12c
Lucas Sena <lucassena.rj@gmail.com>
2020-03-05 08:07:45 +0000

    Fix cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-const path = 'https://api.darksky.net/forecast/ace1850ec41cb58a4bc938b4c440d58d'
+const path = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/ace1850ec41cb58a4bc938b4c440d58d'
 
 const getWeatherInfo = async () => {
   return await axios.get(`${path}/42.3601,-71.0589`, {
     headers: {
-      'Access-Control-Allow-Origin': 'http://localhost:3000',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Origin': '*'
     }
   })
   .then(response => {
